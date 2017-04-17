91549780953bd3e7ceb14f4084b1a7bc486a9bcc
Jessica Hua <jessicahua95@gmail.com>
2017-04-16 19:45:47 -0500

    Node.js added
    To deploy

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/pages/index.html'));
+});
+
+app.get('/about', function(request, response) {
+    response.sendFile(path.join(__dirname + '/pages/about-me.html'))
+});
+
+app.get('/project', function(request, response) {
+    response.sendFile(path.join(__dirname + '/pages/project.html'))
+});
+
+app.get('/resume', function(request, response) {
+    response.sendFile(path.join(__dirname + '/pages/resume.html'))
+});
+
+app.get('/contact', function(request, response) {
+    response.sendFile(path.join(__dirname + '/pages/contact.html'))
+});
+
+app.use(express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
